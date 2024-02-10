7802fdb9a4f225541b1f2567daf17692f5e79376
todilon <120230985+todilon@users.noreply.github.com>
2024-02-09 21:13:15 -0300

    Update dswa_aula_01.py

--- dswa_aula_01.py
+++ dswa_aula_01.py
@@ -1,3 +1,6 @@
+from flask import Flask
+app = Flask(_name_)
+@app.route('/')
 print("Hello from Flask! \n")
 print("\033[1m" + "Aluno: " "\033[0m" + "Thiago Odilon Mariano Da Silva")
 print("\033[1m" + "Prontuário: " "\033[0m" + "PT3019969")
